d1cf4a2cbd1999d9dd0d100d19c38c73fa45dd76
Your Name <you@example.com>
2025-06-11 21:52:41 -0300

    [A02] Atividade 1 - objetos

---
+++ aula_2/atividade_1_objeto.js
@@ -0,0 +1,17 @@
+/*
+    Crie um objeto chamado livro, utilizando a estrutura abaixo:
+        let livro = {
+        titulo: "Dom Quixote",
+        autor: "Miguel de Cervantes"
+        };
+    À esse objeto, adicione a propriedade ‘anoPublicacao’ com o valor 1605 e exiba o objeto completo no console;
+*/
+
+let livro = {
+  titulo: "Dom Quixote",
+  autor: "Miguel de Cervantes"
+};
+
+livro.anoPublicacao = 1605;
+
+console.log(livro);
--- aula_2/atividade_6.js
+++ aula_2/atividade_6.js
@@ -1,6 +1,9 @@
 /*
-
-
+    Dado o seguinte array de emails:
+        const emails = ["joao@email.com", "ana@email.com", "lucas@email.com"];
+        
+    Realize a iteração na lista de forma a, para cada item, colocar no console a seguinte mensagem:
+    “Enviando e-mail para [email_da_lista]”
 */
 
 const emails = ["joao@email.com", "ana@email.com", "lucas@email.com"];
